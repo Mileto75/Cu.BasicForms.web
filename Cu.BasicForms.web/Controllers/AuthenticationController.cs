7c0ef8eddddbd6c7a4edf5a15d068292945bd613
Mileto Di Marco <mileto.di.marco@howest.be>
2024-10-17 12:01:19 +0200

    Add login form

--- Cu.BasicForms.web/Controllers/AuthenticationController.cs
+++ Cu.BasicForms.web/Controllers/AuthenticationController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cu.BasicForms.web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cu.BasicForms.web.Controllers
 {
     public class AuthenticationController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
+            //show the login form
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(AuthenticationLoginViewModel authenticationLoginViewModel)
+        {
+            //process the form
             return View();
         }
     }
---
+++ Cu.BasicForms.web/ViewModels/AuthenticationLoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cu.BasicForms.web.ViewModels
+{
+    public class AuthenticationLoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}

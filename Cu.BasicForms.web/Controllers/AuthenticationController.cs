042f4d3c9cd37de6f13af0ced9a5dce8fe5bf298
Mileto Di Marco <mileto.di.marco@howest.be>
2024-10-17 11:32:28 +0200

    Add project files.

---
+++ Cu.BasicForms.web/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cu.BasicForms.web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
